dbd4e6fe4a5cf77351264abcffbc65f076f8efe3
ashinnotfound <ashinnotfound@qq.com>
2023-09-11 11:27:20 +0800

    init

---
+++ src/main/java/com/ashin/wechatsdk/WechatSdkApplication.java
@@ -0,0 +1,13 @@
+package com.ashin.wechatsdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatSdkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WechatSdkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashin/wechatsdk/WechatSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashin.wechatsdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WechatSdkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
